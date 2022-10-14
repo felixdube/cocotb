a75c297a907f90989a4766c86f33e1f75d76ae0d
Félix Dube <fdube@drwholdings.com>
2022-10-14 13:16:29 +0000

    Add busMonitor

--- tests/test_cases/test_sv_interface/test_sv_if.py
+++ tests/test_cases/test_sv_interface/test_sv_if.py
@@ -3,22 +3,30 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import cocotb
+from cocotb_bus.monitors import BusMonitor
 
+class bfm(BusMonitor):
+    _signals = ["a"]
+    def __init__(self, *args, **kwargs):
+        BusMonitor.__init__(self, *args, **kwargs)
+        assert self.bus.a is not None
+
+    async def _monitor_recv(self):
+        pass
 
 @cocotb.test()
 async def test_sv_if(dut):
     """Test that signals in an interface are discovered and iterable"""
 
+    my_bfm1 = bfm(entity=dut.sv_if_i, clock=dut.clock, name="") 
+    my_bfm2 = bfm(entity=dut.sub_module_inst.sv_if_p, clock=dut.clock, name="")
+
     dut.sv_if_i._discover_all()
     assert hasattr(dut.sv_if_i, "a")
     assert hasattr(dut.sv_if_i, "b")
     assert hasattr(dut.sv_if_i, "c")
-    assert hasattr(dut.sv_if_i, "data")
-    assert dut.sv_if_i.data.value == 0
     
     dut.sub_module_inst.sv_if_p._discover_all()
     assert hasattr(dut.sub_module_inst.sv_if_p, "a")
     assert hasattr(dut.sub_module_inst.sv_if_p, "b")
     assert hasattr(dut.sub_module_inst.sv_if_p, "c")
-    assert hasattr(dut.sub_module_inst.sv_if_p, "data")
-    assert dut.sub_module_inst.sv_if_p.data.value == 0
--- tests/test_cases/test_sv_interface/top.sv
+++ tests/test_cases/test_sv_interface/top.sv
@@ -8,20 +8,15 @@ interface sv_if();
   logic a;
   reg b;
   wire c;
- 
-  `include "./sv_if_include.sv"
-
-  modport source(
-    output data
-  );
 endinterface
 
 module sub_module (
-  sv_if.source sv_if_p
+  sv_if sv_if_p
 );
 endmodule
 
 module top ();
+  logic clock = 1'b0;
   sv_if sv_if_i();
   sub_module sub_module_inst(.sv_if_p(sv_if_i));
 endmodule
