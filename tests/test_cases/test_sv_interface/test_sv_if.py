5d675e67a8adaa0b3c2195bfb30fe24f94f6cebf
Félix Dube <fdube@drwholdings.com>
2022-10-14 12:07:29 +0000

    Enhance test_sv_interface in order to verify access of signal inside interface part of a port map

--- tests/test_cases/test_sv_interface/test_sv_if.py
+++ tests/test_cases/test_sv_interface/test_sv_if.py
@@ -13,3 +13,12 @@ async def test_sv_if(dut):
     assert hasattr(dut.sv_if_i, "a")
     assert hasattr(dut.sv_if_i, "b")
     assert hasattr(dut.sv_if_i, "c")
+    assert hasattr(dut.sv_if_i, "data")
+    assert dut.sv_if_i.data.value == 0
+    
+    dut.sub_module_inst.sv_if_p._discover_all()
+    assert hasattr(dut.sub_module_inst.sv_if_p, "a")
+    assert hasattr(dut.sub_module_inst.sv_if_p, "b")
+    assert hasattr(dut.sub_module_inst.sv_if_p, "c")
+    assert hasattr(dut.sub_module_inst.sv_if_p, "data")
+    assert dut.sub_module_inst.sv_if_p.data.value == 0
--- tests/test_cases/test_sv_interface/top.sv
+++ tests/test_cases/test_sv_interface/top.sv
@@ -8,10 +8,20 @@ interface sv_if();
   logic a;
   reg b;
   wire c;
+ 
+  `include "./sv_if_include.sv"
+
+  modport source(
+    output data
+  );
 endinterface
 
+module sub_module (
+  sv_if.source sv_if_p
+);
+endmodule
+
 module top ();
-
-sv_if sv_if_i();
-
+  sv_if sv_if_i();
+  sub_module sub_module_inst(.sv_if_p(sv_if_i));
 endmodule
